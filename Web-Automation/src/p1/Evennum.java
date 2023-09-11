8015362049e4dddfdb74fe666648a775ddc5d1fe
kolkararif <arif.kolkar@gmail.com>
2023-09-11 22:19:01 +0530

    Recursion Method

---
+++ Web-Automation/src/p1/Evennum.java
@@ -0,0 +1,18 @@
+package p1;
+
+public class Evennum {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+		       int i=100;
+		       while(i<=200)
+		     {
+		        if(i%2==0)
+		        System.out.println("the valu of i is:" +i);
+		        i++;
+
+		      }
+		}
+		}
+
+}
