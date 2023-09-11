aceedc66aa66216c28502aa7bb354229fe355e52
kolkararif <arif.kolkar@gmail.com>
2023-09-11 21:56:35 +0530

    Recursion Method

---
+++ Web-Automation/src/p1/StringLowerUpperCase.java
@@ -0,0 +1,20 @@
+package p1;
+
+public class StringLowerUpperCase {
+	public static void main(String[] args) {
+		StringLowerUpperCase();
+	}
+		// TODO Auto-generated method stub
+		private static void StringLowerUpperCase()
+		{
+			String s1="arif";
+			System.out.println(":"+s1.toUpperCase());
+			String s2="ARIF";
+				System.out.println(":"+s2.toLowerCase());
+		
+
+	}
+
+	
+
+}
