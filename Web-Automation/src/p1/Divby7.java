501a36c6770202161dcdfdbb30f7039c7c8360f6
kolkararif <arif.kolkar@gmail.com>
2023-09-11 22:56:47 +0530

    Recursion Method

---
+++ Web-Automation/src/p1/Divby7.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Divby7 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=100;
+	       while(i>=1)
+	     {
+	        if(i%7==0)
+	        System.out.println("the valu of i is:" +i);
+	        i--;
+
+	      }
+	}
+
+}
