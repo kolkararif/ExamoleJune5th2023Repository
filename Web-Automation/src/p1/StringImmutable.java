1eb9a030e6d66d0c621b441dca159f06b7d1d4a2
kolkararif <arif.kolkar@gmail.com>
2023-09-12 00:22:10 +0530

    Recursion Method

---
+++ Web-Automation/src/p1/StringImmutable.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class StringImmutable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="hello diwakar";
+		s1.concat("fine");
+		System.out.println(":"+ " "+s1);
+		
+		
+		
+
+	}
+
+
+	}
+
+
